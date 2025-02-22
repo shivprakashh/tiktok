0b99fb0ab2bc3144636e564095c41a2da0e3f617
shivprakashh <shivpraksh518@gmail.com>
2025-02-22 23:25:26 +0630

    docker chrome2

--- index.js
+++ index.js
@@ -203,10 +203,10 @@ async function a(user) {
 
     // Wait for the element to be available in the DOM
 
-    await p.waitForSelector('.css-1qb12g8-DivThreeColumnContainer.eegew6e2', { visible: true });
+    await p.waitForSelector('.css-gamknx-DivVideoFeedV2 .ecyq5ls0', { visible: true });
     // Select the element
   
-    const classname = await p.$('.css-1qb12g8-DivThreeColumnContainer.eegew6e2');
+    const classname = await p.$('css-gamknx-DivVideoFeedV2 .ecyq5ls0');
 
     if (classname) {
       // Get the HTML content of the element
