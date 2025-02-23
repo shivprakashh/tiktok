d6a1c9bbc8cd9a1814e9f20bc84d1bd0c6b05db6
shivprakashh <shivpraksh518@gmail.com>
2025-02-23 14:42:01 +0630

    i dont

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ async function a(user) {
     } else {
       console.log('Browser is closed');
     }
-    const initialHTML = await page.evaluate(() => document.body.innerHTML);
+    const initialHTML = await p.evaluate(() => document.body.innerHTML);
     console.log("Initial Page HTML:", initialHTML.substring(0, 2000)); // Log first 2000 characters to avoid overflow
 
     // Wait for the class containing images and links to be available
